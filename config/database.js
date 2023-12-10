8cd2de477d3c4495ca6aba48ca73b6253a26a684
raihan4453 <raihan.mauludana@students.amikom.ac.id>
2023-12-10 16:47:36 +0700

    ganti connection database

--- config/database.js
+++ config/database.js
@@ -1,8 +1,21 @@
 import { Sequelize } from "sequelize"
+import dotenv from "dotenv"
 
-const db = new Sequelize('healthnews', 'root', '1201',{
-    host: 'localhost',
-    dialect: 'mysql'
+dotenv.config()
+
+const {
+    DB_URL
+} = process.env
+
+// const db = new Sequelize('healthnews', 'root', '1201',{
+//     host: 'localhost',
+//     dialect: 'mysql'
+// })
+
+const db = new Sequelize( DB_URL, {
+    define: {
+        timestamps: false
+    }
 })
 
 export default db
